8cf9f04df6a3a721cdc7cb037e5481546acbc9be
Viet Pham <vietpham1097@gmail.com>
2021-09-11 23:11:11 +0700

    Define Tank Blueprint child components in c++

--- Source/BattleTank/Pawn/Tank.cpp
+++ Source/BattleTank/Pawn/Tank.cpp
@@ -2,13 +2,62 @@
 
 
 #include "Tank.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Camera/CameraTypes.h"
+#include "Components/ArrowComponent.h"
+#include "Components/StaticMeshComponent.h"
 #include "Component/TankAimingComponent.h"
+#include "Component/TankTurret.h"
+#include "Component/TankBarrel.h"
 
 // Sets default values
 ATank::ATank()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
+
+	if (!RootComponent) 
+	{
+		RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Tank"));
+	}
+
+	TankDirection = CreateDefaultSubobject<UArrowComponent>(TEXT("Direction"));
+	TankDirection->AttachTo(RootComponent);
+
+	TankBody = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Body"));
+	TankBody->SetSimulatePhysics(true);
+	TankBody->SetMassOverrideInKg(TEXT("Mass In Kg"), 40000.0f);
+	TankBody->AttachTo(TankDirection);
+
+	TankTrackLeft = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TrackLeft"));
+	TankTrackLeft->AttachTo(TankBody, "Left Track");
+
+	TankTrackRight = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TrackRight"));
+	TankTrackRight->AttachTo(TankBody, "Right Track");
+
+	TankTurret = CreateDefaultSubobject<UTankTurret>(TEXT("Turret"));
+	TankTurret->AttachTo(TankBody, "Turret");
+
+	TankBarrel = CreateDefaultSubobject<UTankBarrel>(TEXT("Barrel"));
+	TankBarrel->AttachTo(TankTurret, "Barrel");
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->TargetArmLength = 500.0f;
+	SpringArm->bEnableCameraLag = false;
+	SpringArm->bEnableCameraRotationLag = false;
+	SpringArm->bUsePawnControlRotation = false;
+	SpringArm->bDoCollisionTest = false;
+	SpringArm->bInheritRoll = false;
+	SpringArm->AttachTo(TankBody);
+	SpringArm->SetRelativeLocation(FVector(0.0f, 0.0f, 150.0f));
+	SpringArm->SetRelativeRotation(FRotator(-20.0f, 0.0f, 0.0f));
+
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	CameraComponent->bUsePawnControlRotation = false;
+	CameraComponent->ProjectionMode = ECameraProjectionMode::Perspective;
+	CameraComponent->AttachTo(SpringArm, USpringArmComponent::SocketName);
+	CameraComponent->SetRelativeLocation(FVector(-600.0f, 0.0f, 0.0f));
 
 	AimingComponent = CreateDefaultSubobject<UTankAimingComponent>("AimingComponent");
 }
@@ -17,7 +66,9 @@ ATank::ATank()
 void ATank::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	AimingComponent->SetTurretComponent(TankTurret);
+	AimingComponent->SetBarrelComponent(TankBarrel);
 }
 
 // Called to bind functionality to input
@@ -30,15 +81,5 @@ void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 void ATank::AimAt(FVector HitLocation)
 {
 	this->AimingComponent->AimAt(HitLocation, this->LaunchSpeed);
-}
-
-void ATank::SetTurretComponent(UTankTurret* Turret)
-{
-	this->AimingComponent->SetTurretComponent(Turret);
-}
-
-void ATank::SetBarrelComponent(UTankBarrel* Barrel)
-{
-	this->AimingComponent->SetBarrelComponent(Barrel);
 }
 
--- Source/BattleTank/Pawn/Tank.h
+++ Source/BattleTank/Pawn/Tank.h
@@ -6,6 +6,11 @@
 #include "GameFramework/Pawn.h"
 #include "Tank.generated.h"
 
+class UArrowComponent;
+class USpringArmComponent;
+class UCameraComponent;
+class UTankAimingComponent;
+class UStaticMeshComponent;
 class UTankTurret;
 class UTankBarrel;
 
@@ -15,11 +20,43 @@ class BATTLETANK_API ATank : public APawn
 	GENERATED_BODY()
 
 private:
+	// Helpful debug tool - which way is the tank facing?
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Tank", meta = (AllowPrivateAccess = "true"))
-	class UTankAimingComponent* AimingComponent = nullptr;
+	UArrowComponent* TankDirection = nullptr;
+
+	// Static Mesh for the tank body
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Tank", meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* TankBody = nullptr;
+
+	// Static Mesh for the tank track left
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Tank", meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* TankTrackLeft = nullptr;
+
+	// Static Mesh for the tank track right
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Tank", meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* TankTrackRight = nullptr;
+
+	// Static Mesh for the tank turret
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Tank", meta = (AllowPrivateAccess = "true"))
+	UTankTurret* TankTurret = nullptr;
+
+	// Static Mesh for the tank barrel
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Tank", meta = (AllowPrivateAccess = "true"))
+	UTankBarrel* TankBarrel = nullptr;
+
+	// Spring-Arm for the camera
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Tank", meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* SpringArm = nullptr;
+
+	// Third-person camera
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Tank", meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* CameraComponent = nullptr;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "TankAiming", meta = (AllowPrivateAccess = "true"))
+	UTankAimingComponent* AimingComponent = nullptr;
 
 	UPROPERTY(EditAnywhere, Category = "Firing")
-	float LaunchSpeed = 800000; // 8000 m/s ~ 800,000 cm/s
+	float LaunchSpeed = 100000; // 1000 m/s ~ 100,000 cm/s
 
 public:
 	// Sets default values for this pawn's properties
@@ -34,10 +71,4 @@ public:
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 	void AimAt(FVector HitLocation);
-
-	UFUNCTION(BlueprintCallable, Category = Setup)
-	void SetTurretComponent(UTankTurret* Turret);
-
-	UFUNCTION(BlueprintCallable, Category = Setup)
-	void SetBarrelComponent(UTankBarrel* Barrel);
 };
